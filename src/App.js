ad7c7838561bb96b61bbc1a5977dc8a353527f59
= <=>
2016-11-30 21:46:21 -0800

    React player architechture partially done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React  from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import './App.css';
+import RadioPlayer from './ReactPlayer';
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <MuiThemeProvider>
+          <RadioPlayer  />
+        </MuiThemeProvider>
       </div>
     );
   }
