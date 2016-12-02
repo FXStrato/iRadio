fe1b1e1b08c52a4bcb6130e54db94d69a3c33012
Eric Wei <ericjwei@gmail.com>
2016-12-01 16:56:47 -0800

    firebase config

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBVGyJq21xT2zRpbNMqBAtvq9haT7gMI08",
+    authDomain: "iradio-614db.firebaseapp.com",
+    databaseURL: "https://iradio-614db.firebaseio.com",
+    storageBucket: "iradio-614db.appspot.com",
+    messagingSenderId: "552716146199"
+  };
+  firebase.initializeApp(config);
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
